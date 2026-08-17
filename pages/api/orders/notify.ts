c0c838345e14735633c42b6434679b417e2068b1
kaartik00k-tech <kaartik.00k@gmail.com>
2026-08-17 16:04:26 +0530

    Move pages and lib at build time (prebuild script) and use path alias for notify import

--- pages/api/orders/notify.ts
+++ pages/api/orders/notify.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { notifyNewOrder } from '../../lib/twilio-notify';
+import { notifyNewOrder } from '@/lib/twilio-notify';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
